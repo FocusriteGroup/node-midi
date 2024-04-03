af6549d0b9df2cd7a6e7a4ae04ee3c46afadae39
Loz Jackson <lozjackson@users.noreply.github.com>
2024-04-03 14:18:14 +0100

    refactor

--- binding.gyp
+++ binding.gyp
@@ -1,76 +1,76 @@
 {
-  'targets': [
-    {
-      'target_name': 'midi',
-      'include_dirs': [
-        "<!(node -e \"require('nan')\")",
-        'src',
-        'vendor/rtmidi'
-      ],
-      'sources': [
-        'vendor/rtmidi/RtMidi.cpp',
-        'src/input.cpp',
-        'src/output.cpp',
-        'src/midi.cpp'
-      ],
-      'conditions': [
-        ['OS=="linux"',
-          {
-            'cflags_cc!': [
-              '-fno-exceptions'
-            ],
-            'defines': [
-              '__LINUX_ALSA__'
-            ],
-            'link_settings': {
-              'libraries': [
-                '-lasound',
-                '-lpthread',
-              ]
-            }
-          }
-        ],
-        ['OS=="mac"',
-          {
-            'defines': [
-              '__MACOSX_CORE__'
-            ],
-            'xcode_settings': {
-              'GCC_ENABLE_CPP_EXCEPTIONS': 'YES'
+    "targets": [
+        {
+            "target_name": "midi",
+            "cflags!": ["-fexceptions"],
+            "cflags_cc!": ["-fexceptions"],
+            "xcode_settings": {
+                "GCC_ENABLE_CPP_EXCEPTIONS": "YES",
+                "CLANG_CXX_LIBRARY": "libc++",
+                "MACOSX_DEPLOYMENT_TARGET": "10.7",
             },
-            'link_settings': {
-              'libraries': [
-                'CoreMIDI.framework',
-                'CoreAudio.framework',
-                'CoreFoundation.framework',
-              ],
-            }
-          }
-        ],
-        ['OS=="win"',
-          {
-            'configurations': {
-              'Release': {
-                'msvs_settings': {
-                  'VCCLCompilerTool': {
-                    'ExceptionHandling': 1
-                  }
-                }
-              }
+            "msvs_settings": {
+                "VCCLCompilerTool": {"ExceptionHandling": 1},
             },
-            'defines': [
-              '__WINDOWS_MM__',
-              'RT_SYSEX_BUFFER_SIZE=16384',
-              'RTMIDI_DO_NOT_ENSURE_UNIQUE_PORTNAMES'
+            "include_dirs": [
+                "<!(node -p \"require('node-addon-api').include_dir\")",
+                "src",
+                "vendor/rtmidi",
             ],
-            'link_settings': {
-              'libraries': [
-                '-lwinmm.lib'
-              ],
-            }
-          }
-        ]
-      ]
-    }
-  ]
+            "sources": [
+                "vendor/rtmidi/RtMidi.cpp",
+                "src/input.cpp",
+                "src/output.cpp",
+                "src/midi.cpp",
+            ],
+            "conditions": [
+                [
+                    'OS=="linux"',
+                    {
+                        "cflags_cc!": ["-fno-exceptions"],
+                        "defines": ["__LINUX_ALSA__"],
+                        "link_settings": {
+                            "libraries": [
+                                "-lasound",
+                                "-lpthread",
+                            ]
+                        },
+                    },
+                ],
+                [
+                    'OS=="mac"',
+                    {
+                        "defines": ["__MACOSX_CORE__"],
+                        "xcode_settings": {"GCC_ENABLE_CPP_EXCEPTIONS": "YES"},
+                        "link_settings": {
+                            "libraries": [
+                                "CoreMIDI.framework",
+                                "CoreAudio.framework",
+                                "CoreFoundation.framework",
+                            ],
+                        },
+                    },
+                ],
+                [
+                    'OS=="win"',
+                    {
+                        "configurations": {
+                            "Release": {
+                                "msvs_settings": {
+                                    "VCCLCompilerTool": {"ExceptionHandling": 1}
+                                }
+                            }
+                        },
+                        "defines": [
+                            "__WINDOWS_MM__",
+                            "RTMIDI_DO_NOT_ENSURE_UNIQUE_PORTNAMES",
+                        ],
+                        "link_settings": {
+                            "libraries": ["-lwinmm.lib"],
+                        },
+                    },
+                ],
+            ],
+        }
+    ]
 }
--- src/output.cpp
+++ src/output.cpp
@@ -1,194 +1,31 @@
-#include <nan.h>
+#ifndef NODE_MIDI_OUTPUT_H
+#define NODE_MIDI_OUTPUT_H
+
+#include <napi.h>
 
 #include "RtMidi.h"
 
-#include "output.h"
-
-void NodeMidiOutput::Init(v8::Local<v8::Object> target)
+class NodeMidiOutput : public Napi::ObjectWrap<NodeMidiOutput>
 {
-    Nan::HandleScope scope;
+private:
+    std::unique_ptr<RtMidiOut> handle;
 
-    v8::Local<v8::FunctionTemplate> t = Nan::New<v8::FunctionTemplate>(NodeMidiOutput::New);
+public:
+    static std::unique_ptr<Napi::FunctionReference> Init(const Napi::Env &env, Napi::Object target);
 
-    s_ct.Reset(t);
-    t->SetClassName(Nan::New<v8::String>("NodeMidiOutput").ToLocalChecked());
-    t->InstanceTemplate()->SetInternalFieldCount(1);
+    NodeMidiOutput(const Napi::CallbackInfo &info);
+    ~NodeMidiOutput();
 
-    Nan::SetPrototypeMethod(t, "getPortCount", GetPortCount);
-    Nan::SetPrototypeMethod(t, "getPortName", GetPortName);
+    Napi::Value GetPortCount(const Napi::CallbackInfo &info);
+    Napi::Value GetPortName(const Napi::CallbackInfo &info);
 
-    Nan::SetPrototypeMethod(t, "openPort", OpenPort);
-    Nan::SetPrototypeMethod(t, "openVirtualPort", OpenVirtualPort);
-    Nan::SetPrototypeMethod(t, "closePort", ClosePort);
-    Nan::SetPrototypeMethod(t, "release", Release);
+    Napi::Value OpenPort(const Napi::CallbackInfo &info);
+    Napi::Value OpenVirtualPort(const Napi::CallbackInfo &info);
+    Napi::Value ClosePort(const Napi::CallbackInfo &info);
+    Napi::Value Destroy(const Napi::CallbackInfo &info);
+    Napi::Value IsPortOpen(const Napi::CallbackInfo &info);
 
-    Nan::SetPrototypeMethod(t, "isPortOpen", IsPortOpen);
+    Napi::Value Send(const Napi::CallbackInfo &info);
+};
 
-    Nan::SetPrototypeMethod(t, "sendMessage", Send);
-    Nan::SetPrototypeMethod(t, "send", Send);
-
-    Nan::Set(target, Nan::New<v8::String>("Output").ToLocalChecked(), Nan::GetFunction(t).ToLocalChecked());
-}
-
-NodeMidiOutput::NodeMidiOutput()
-{
-    try {
-        out = new RtMidiOut();
-    }
-    catch(RtMidiError &e) {
-        out = NULL;
-    }
-}
-
-NodeMidiOutput::~NodeMidiOutput()
-{
-    if (out) {
-        delete out;
-    }
-}
-
-NAN_METHOD(NodeMidiOutput::New)
-{
-    Nan::HandleScope scope;
-
-    if (!info.IsConstructCall()) {
-        return Nan::ThrowTypeError("Use the new operator to create instances of this object.");
-    }
-
-    NodeMidiOutput* output = new NodeMidiOutput();
-    output->Wrap(info.This());
-
-    info.GetReturnValue().Set(info.This());
-}
-
-NAN_METHOD(NodeMidiOutput::Release)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-
-    if (output->out) {
-        delete output->out;
-        output->out = nullptr;
-    }
-}
-
-
-NAN_METHOD(NodeMidiOutput::GetPortCount)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-    v8::Local<v8::Integer> result = Nan::New<v8::Uint32>(output-> out ? output->out->getPortCount() : 0);
-    info.GetReturnValue().Set(result);
-}
-
-NAN_METHOD(NodeMidiOutput::GetPortName)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-    if (info.Length() == 0 || !info[0]->IsUint32()) {
-        return Nan::ThrowTypeError("First argument must be an integer");
-    }
-
-    unsigned int portNumber = Nan::To<unsigned int>(info[0]).FromJust();
-    try {
-        v8::Local<v8::String> result = Nan::New<v8::String>(output->out ? output->out->getPortName(portNumber).c_str() : "").ToLocalChecked();
-        info.GetReturnValue().Set(result);
-    }
-    catch(RtMidiError& e) {
-        info.GetReturnValue().Set(Nan::New<v8::String>("").ToLocalChecked());
-    }
-}
-
-NAN_METHOD(NodeMidiOutput::OpenPort)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-
-    if (!output->out) return;
-
-    if (info.Length() == 0 || !info[0]->IsUint32()) {
-        return Nan::ThrowTypeError("First argument must be an integer");
-    }
-    unsigned int portNumber = Nan::To<unsigned int>(info[0]).FromJust();
-    if (portNumber >= output->out->getPortCount()) {
-        return Nan::ThrowRangeError("Invalid MIDI port number");
-    }
-
-    try {
-        output->out->openPort(portNumber);
-    }
-    catch(RtMidiError& e) {
-        ;
-    }
-    return;
-}
-
-NAN_METHOD(NodeMidiOutput::OpenVirtualPort)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-
-    if (!output->out) return;
-
-    if (info.Length() == 0 || !info[0]->IsString()) {
-        return Nan::ThrowTypeError("First argument must be a string");
-    }
-
-    std::string name(*Nan::Utf8String(info[0]));
-
-    try {
-        output->out->openVirtualPort(name);
-    }
-    catch(RtMidiError& e) {
-        ;
-    }
-    return;
-}
-
-NAN_METHOD(NodeMidiOutput::ClosePort)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-
-    if (!output->out) return;
-
-    output->out->closePort();
-    return;
-}
-
-NAN_METHOD(NodeMidiOutput::IsPortOpen)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-
-    if (!output->out) return;
-
-    v8::Local<v8::Boolean> result = Nan::New<v8::Boolean>(output->out->isPortOpen());
-    info.GetReturnValue().Set(result);
-}
-
-NAN_METHOD(NodeMidiOutput::Send)
-{
-    Nan::HandleScope scope;
-    NodeMidiOutput* output = Nan::ObjectWrap::Unwrap<NodeMidiOutput>(info.This());
-
-    if (!output->out) return;
-
-    if (info.Length() == 0 || !info[0]->IsArray()) {
-        return Nan::ThrowTypeError("First argument must be an array");
-    }
-
-    v8::Local<v8::Object> message = Nan::To<v8::Object>(info[0]).ToLocalChecked();
-    int32_t messageLength = Nan::To<int32_t>(Nan::Get(message, Nan::New<v8::String>("length").ToLocalChecked()).ToLocalChecked()).FromJust();
-    std::vector<unsigned char> messageOutput;
-    for (int32_t i = 0; i != messageLength; ++i) {
-        messageOutput.push_back(Nan::To<unsigned int>(Nan::Get(message, Nan::New<v8::Integer>(i)).ToLocalChecked()).FromJust());
-    }
-    try {
-        output->out->sendMessage(&messageOutput);
-    }
-    catch(RtMidiError& e) {
-        ;
-    }
-    return;
-}
+#endif // NODE_MIDI_OUTPUT_H
